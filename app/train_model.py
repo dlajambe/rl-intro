2072b4f3023310e01b15fef90e991d2871d87174
David LaJambe <dlajambe@gmail.com>
2023-12-05 21:59:57 -0500

    Add automatic directory creation

--- app/train_model.py
+++ app/train_model.py
@@ -5,15 +5,34 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
+def make_dir(dir: str):
+    """Creates a folder at the provided directory if one does not
+    already exist.
+
+    Parameters
+    ----------
+
+    dir : str
+        The location of the directory that should be created.
+    """
+    if os.path.isdir(dir) == False:
+        os.mkdir(dir)
+
 def main():
     """Entry point into the program.
     """
-    env_name = 'CartPole-v1'
+    output_dir = 'output/'
+    log_dir = output_dir + 'logs/'
+    model_dir = output_dir + '/models'
+    make_dir(output_dir)
+    make_dir(log_dir)
+    make_dir(model_dir)
+
+    env_id = 'CartPole-v1'
     render_mode = 'human'
-    env = gym.make(env_name, render_mode=render_mode)
+    env = gym.make(id=env_id, render_mode=render_mode)
 
     n_episodes = 5
-
     for i in range(n_episodes):
         terminated = False
         state = env.reset()
